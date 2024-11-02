828fcf6ff75455a9dee4792d6494c070f19b6284
Roslan Saidi <roslansaidi@ymail.com>
2024-11-03 06:29:12 +0800

    Fix link

--- layouts/portal.vue
+++ layouts/portal.vue
@@ -56,7 +56,7 @@
                                       </PopoverButton>
                                 </div>
                                 <div class="hidden lg:flex lg:items-center lg:justify-end xl:col-span-4">
-                                      <NuxtLink to="/sign-up"
+                                      <NuxtLink to="/sign-in"
                                             class="ml-6 inline-flex items-center rounded-md bg-red-900 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">Login</NuxtLink>
                                 </div>
                           </div>
